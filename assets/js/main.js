036fbd646116b5424863e4bb2bac5fe29875c0a6
Lee Grobler <dev@lee-grobler.com>
2025-04-17 22:34:49 +0200

    convert home and sub selectors to web components.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,19 @@
 $(document).ready(() => {
-  chrome.storage.local.get(
-    [
-      "homeSort",
-      "subSort",
-      "userSort",
-      "homeEnabled",
-      "subEnabled",
-      "userEnabled",
-    ],
-    (data) => {
-      $("#homeSort").val(data.homeSort || "hot");
-      $("#subSort").val(data.subSort || "hot");
-      $("#userSort").val(data.userSort || "hot");
-      $("#homeToggle").prop("checked", data.homeEnabled !== false);
-      $("#subToggle").prop("checked", data.subEnabled !== false);
-      $("#userToggle").prop("checked", data.userEnabled !== false);
-    }
-  );
+  console.log("document ready");
+
+  chrome.storage.local.get(["userSort", "userEnabled"], (data) => {
+    $("#userSort").val(data.userSort || "hot");
+    $("#userToggle").prop("checked", data.userEnabled !== false);
+  });
 
   const savePreferences = () => {
     const preferences = {
-      homeSort: $("#homeSort").val(),
-      subSort: $("#subSort").val(),
       userSort: $("#userSort").val(),
-      homeEnabled: $("#homeToggle").prop("checked"),
-      subEnabled: $("#subToggle").prop("checked"),
       userEnabled: $("#userToggle").prop("checked"),
     };
     chrome.storage.local.set(preferences);
   };
 
-  $("#homeSort").on("change", savePreferences);
-  $("#subSort").on("change", savePreferences);
   $("#userSort").on("change", savePreferences);
-
-  $("#homeToggle").on("change", savePreferences);
-  $("#subToggle").on("change", savePreferences);
   $("#userToggle").on("change", savePreferences);
 });
--- components/sort-standard/index.js
+++ components/sort-standard/index.js
@@ -2,6 +2,8 @@ class SortStandard extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
+    const section = this.getAttribute("section");
+    const label = this.getAttribute("label");
 
     loadTemplateAndStyles(
       shadow,
@@ -10,7 +12,34 @@ class SortStandard extends HTMLElement {
       "sort-standard"
     )
       .then((content) => {
-        console.log("component mounted:", window.jQuery);
+        const $sort = $(shadow.querySelector("#sort"));
+        const $toggle = $(shadow.querySelector("#toggle"));
+
+        $(shadow.querySelector(".form-label"))[0].innerHTML = label;
+
+        chrome.storage.local.get(
+          [`${section}Sort`, `${section}Enabled`],
+          (data) => {
+            $sort.val(data[`${section}Sort`] || "hot");
+            $toggle.prop("checked", data[`${section}Enabled`] !== false);
+          }
+        );
+
+        $sort.on("change", () => {
+          const preferences = {
+            [`${section}Sort`]: $sort.val(),
+            [`${section}Enabled`]: $toggle.prop("checked"),
+          };
+          chrome.storage.local.set(preferences);
+        });
+
+        $toggle.on("change", () => {
+          const preferences = {
+            [`${section}Sort`]: $sort.val(),
+            [`${section}Enabled`]: $toggle.prop("checked"),
+          };
+          chrome.storage.local.set(preferences);
+        });
       })
       .catch((err) => console.error("Failed to mount component:", err));
   }
